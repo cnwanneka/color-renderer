1123531512f3755f5dda7a6ac41dd6db71ff10a6
Chidi Nwanneka <cnwanneka@gmail.com>
2023-04-26 23:43:58 +0100

    added color and colorRenderer components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import ColorRenderer from './components/ColorRenderer';
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      
+    <div className='App'>
+      <ColorRenderer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
